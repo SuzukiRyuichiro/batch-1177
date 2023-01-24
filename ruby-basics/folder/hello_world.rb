d5563c9de8b09f24fc79d4f7b1e363280fcf27de
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2023-01-24 11:18:07 +0200

    initial commit

---
+++ ruby-basics/folder/hello_world.rb
@@ -0,0 +1,4 @@
+def some_method?(param)
+  #
+  return true
+end
---
+++ ruby-basics/method.rb
@@ -0,0 +1,25 @@
+
+# def say_hi(name)
+#   return "Hi #{name}!"
+# end
+
+# def full_name(first_name, last_name)
+#   name = "Mr. #{first_name.capitalize} #{last_name.capitalize}"
+#   return name
+# end
+
+scooter_first_name = "Ryuichiro"
+# scooter_last_name = "Suzuki"
+
+# puts full_name(scooter_first_name, scooter_last_name)
+
+require 'date'
+
+def tomorrow_date
+  tomorrow_date = Date.today + 1
+  return tomorrow_date.strftime("%B %d")
+end
+
+firs
+b = 20
+puts tomorrow   # => "October 4"
